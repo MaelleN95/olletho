b2da24e94ffccd4689264c0d997d08e3e3bc75a2
Maelle N <maelle.nioche@gmail.com>
2024-09-28 18:49:13 +0200

    Refactor: Improve game logic and move validation

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -69,11 +69,12 @@ function GameBoard() {
   // Initialize the board
   useEffect(() => {
     initializeBoard();
+    // eslint-disable-next-line
   }, []);
 
   /**
    *  Check if the board is full
-   * @param {Array} boardState The current state of the board
+   * @param {Array<Array<number>>} boardState The current state of the board
    * @returns {boolean} True if the board is full, false otherwise
    */
   const isBoardFull = (boardState) => {
@@ -90,20 +91,26 @@ function GameBoard() {
   /**
    * Check if a player has any valid moves available
    * @param {number} player The player to check
-   * @param {Array} boardState The current state of the board
+   * @param {Array<Array<number>>} boardState The current state of the board
    * @returns {boolean} True if the player has at least one valid move, false otherwise
    */
-  const canPlayerMakeMove = (player) => {
-    // for (let row = 0; row < 8; row++) {
-    //   for (let col = 0; col < 8; col++) {
-    //     if (player === 1 || player === 2) {
-    //       return true;
-    //     }
-    //   }
-    // }
+  const canPlayerMakeMove = (player, boardState) => {
+    for (let row = 0; row < 8; row++) {
+      for (let col = 0; col < 8; col++) {
+        const { validMove } = validMoves(row, col, player, boardState);
+        if (validMove) {
+          return true;
+        }
+      }
+    }
     return true;
   };
 
+  /**
+   * Check who is the winner of the game
+   * @param {Array<Array<number>>} boardState The current state of the board
+   * @returns {Array} An array containing the winner and the number of black and white pieces
+   */
   const checkWinner = (boardState) => {
     let blackPieces = 0;
     let whitePieces = 0;
@@ -128,14 +135,17 @@ function GameBoard() {
   };
 
   /**
-   * Check if the game is over
-   * @param {Array} boardState The current state of the board
+   * Check if the game is over (i.e., the board is full or if both players can't make a move)
+   * @param {Array<Array<number>>} boardState The current state of the board
    * @returns {boolean} True if the game is over, false otherwise
    * */
   const checkGameOver = (boardState) => {
     if (isBoardFull(boardState)) {
       return true;
-    } else if (!canPlayerMakeMove(1) && !canPlayerMakeMove(2)) {
+    } else if (
+      !canPlayerMakeMove(1, boardState) &&
+      !canPlayerMakeMove(2, boardState)
+    ) {
       return true;
     }
     return false; // La partie continue
@@ -156,7 +166,7 @@ function GameBoard() {
    * @param {number} row The row index of the cell
    * @param {number} col The column index of the cell
    * @param {number} player The player making the move
-   * @param {Array} boardState The current state of the board
+   * @param {Array<Array<number>>} boardState The current state of the board
    * @returns {Array} An array of valid directions
    */
   const findValidDirections = (row, col, player, boardState) => {
@@ -194,7 +204,7 @@ function GameBoard() {
    * @param {number} col The column index of the cell
    * @param {Array} direction The direction to check
    * @param {number} player The player making the move
-   * @param {Array} boardState The current state of the board
+   * @param {Array<Array<number>>} boardState The current state of the board
    * @returns {Array} An array of flippable pieces
    */
   const findFlippablePiecesForOneDirection = (
@@ -234,7 +244,7 @@ function GameBoard() {
 
   /**
    * Flip the pieces in the given directions
-   * @param {Array} newBoardState The new state of the board
+   * @param {Array<Array<number>>} newBoardState The new state of the board
    * @param {Array} adversePiecesToFlip The pieces to flip
    * @param {number} player The player making the move
    */
@@ -245,21 +255,30 @@ function GameBoard() {
   };
 
   /**
-   * Check if the move is valid
-   * @param {number} row The row index of the cell where the move is made
-   * @param {number} col The column index of the cell where the move is made
-   * @param {number} player The player making the mov
-   * @returns {boolean} True if the move is valid, false otherwise
+   * Check if a move is valid
+   *
+   * @param {number} row - The row index of the cell being evaluated for a move.
+   * @param {number} col - The column index of the cell being evaluated for a move.
+   * @param {number} player - The identifier for the player making the move (e.g., 1 or 2).
+   * @param {Array<Array<number>>} boardState - The current state of the game board
+   *
+   * @returns {Object} An object indicating whether the move is valid and, if so, the positions of the valid move and any flipped pieces.
+   *                  - validMove: {boolean} Indicates if the move is valid.
+   *                  - validMovesPositions: {Array<number>} The [row, col] of the attempted move if valid.
+   *                  - flippedPieces: {Array<Array<number>>} An array containing arrays of the positions of flipped pieces
    */
-  const isValidMove = (row, col, player) => {
+  const validMoves = (row, col, player, boardState) => {
+    // Check if the cell is out of bounds
+    if (isOutOfBounds(row, col)) {
+      return false;
+    }
+
     // Check if the cell is empty
     if (boardState[row][col] !== 0) {
-      console.log('cell is not empty');
       return false;
     }
 
     // Copy the board state
-    let newBoardState = boardState.map((row) => row.slice());
     let newFlippedPieces = [];
 
     // Find valid directions
@@ -267,7 +286,6 @@ function GameBoard() {
 
     // If there are no valid directions, the move is invalid
     if (validDirections.length === 0) {
-      console.log('Invalid move - no valid directions');
       return false;
     }
 
@@ -286,53 +304,100 @@ function GameBoard() {
       }
     });
 
-    // If there are pieces to flip, execute the move
+    // If there are pieces to flip, the move is valid
     if (newFlippedPieces.length > 0) {
-      // Place the player's piece on the board
-      newBoardState[row][col] = player;
-      // Flip the adverse pieces
-      flipPieces(newBoardState, newFlippedPieces, player);
-
-      // Update the game state
-      setPlayer(3 - player);
-      setLastMove([row, col]);
-      setFlippedPieces(newFlippedPieces);
-      setBoardState(newBoardState);
-      return true;
+      return {
+        validMove: true,
+        validMovesPositions: [row, col],
+        flippedPieces: [newFlippedPieces],
+      };
     } else {
-      console.log('Invalid move - no pieces to flip');
-      return false;
+      return { validMove: false };
     }
   };
 
   /**
-   * Process the move
-   * @param {number} row The row index of the cell where the move is made
-   * @param {number} col The column index of the cell where the move is made
+   * Execute the move for the current player
+   * @param {number} row The row index where the player wants to move
+   * @param {number} col The column index where the player wants to move
+   * @param {number} player The player making the move
+   * @returns {Object} An object indicating whether the move was executed and the new board state
+   */
+  const executeMove = (row, col, player) => {
+    // Check if the move is valid and get the list of flippable pieces
+    const { validMove, flippedPieces } = validMoves(
+      row,
+      col,
+      player,
+      boardState
+    );
+
+    // If the move is invalid, do nothing
+    if (!validMove) {
+      return { moveExecuted: false };
+    }
+
+    // Make a copy of the current board state
+    let newBoardState = boardState.map((row) => row.slice());
+
+    // Place the player's piece on the selected cell
+    newBoardState[row][col] = player;
+
+    // Flip the pieces
+    flipPieces(newBoardState, flippedPieces[0], player);
+
+    // Update the board state
+    setBoardState(newBoardState);
+
+    // Store the last move
+    setLastMove([row, col]);
+
+    // Set the flipped pieces for visual effect
+    setFlippedPieces(flippedPieces[0]);
+
+    return { moveExecuted: true, newBoardState };
+  };
+
+  /**
+   * Manage the player turn logic
+   * @param {number} row The row index where the player wants to move
+   * @param {number} col The column index where the player wants to move
    */
   const managePlayerMove = (row, col) => {
-    if (canPlayerMakeMove(player)) {
-      isValidMove(row, col, player, boardState);
+    // Try to execute the game logic for the current player
+    const { moveExecuted, newBoardState } = executeMove(row, col, player);
+
+    // If the move was successful, attempt to switch to the other player
+    if (moveExecuted) {
+      const nextPlayer = 3 - player; // Switch to the other player
       // Check if the game is over
-      if (checkGameOver(boardState)) {
-        console.log('Game Over - Board is full');
+      const gameOver = checkGameOver(newBoardState);
+
+      if (gameOver) {
+        console.log('Game Over');
         const [winner, blackPieces, whitePieces] = checkWinner(boardState);
         console.log('Winner is ', winner);
         console.log('Black pieces: ', blackPieces);
         console.log('White pieces: ', whitePieces);
+        return;
       }
-    } else if (!canPlayerMakeMove(1) && !canPlayerMakeMove(2)) {
-      console.log('Game Over - No valid moves for both players');
-      const [winner, blackPieces, whitePieces] = checkWinner(boardState);
-      console.log('Winner is ', winner);
-      console.log('Black pieces: ', blackPieces);
-      console.log('White pieces: ', whitePieces);
-    } else if (!canPlayerMakeMove(player)) {
-      console.log(
-        'No valid moves for player ',
-        player === 1 ? 'black' : 'white'
-      );
-      setPlayer(3 - player);
+
+      // Check if the next player has any valid moves
+      if (canPlayerMakeMove(nextPlayer, newBoardState)) {
+        // Switch to the next player if they can move
+        setPlayer(nextPlayer);
+      } else if (canPlayerMakeMove(player, newBoardState)) {
+        // If the next player can't move, keep the current player
+        console.log(
+          `Player ${
+            nextPlayer === 1 ? 'black' : 'white'
+          } has no valid moves. Player ${
+            player === 1 ? 'black' : 'white'
+          } continues.`
+        );
+      }
+    } else {
+      console.log('Invalid move');
     }
   };
 
